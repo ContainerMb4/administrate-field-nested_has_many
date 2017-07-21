a7448d0ea98f64d45d7401d57b5ea0eedf41ae0e
Elliott Hebert <elliott.hebert.pro@gmail.com>
2017-07-21 18:05:47 +0200

    make datetimepicker work with nested field

--- app/assets/javascripts/administrate-field-nested_has_many/application.js
+++ app/assets/javascripts/administrate-field-nested_has_many/application.js
@@ -1,1 +1,14 @@
 //= require cocoon
+
+// this is making the datetimepicker work with nested fields
+// TODO: shared datetimepicker configuration
+
+$(document).ready(function() {
+  $('*')
+    .on('cocoon:after-insert', function(e, added_task) {
+      $(added_task.find(".datetimepicker")).datetimepicker({
+          debug: false,
+          format: "YYYY-MM-DD HH:mm:ss",
+        });
+  });
+});
